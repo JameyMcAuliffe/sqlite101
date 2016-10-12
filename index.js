b00afd0fb05ee694887e5cae9440308c7897763a
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-10-12 13:22:16 -0500

    table headers only show once

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cliTable = require('cli-table')
 
 const db = new Database('db/Chinook_Sqlite.sqlite')
 
+
 db.serialize(() => {
 	// db.all(`
 	// 	SELECT FirstName || ' ' || LastName AS 'Name', CustomerId, Country FROM Customer
@@ -18,26 +19,41 @@ db.serialize(() => {
 	// 	, (err, customers) => {
 	// 		console.log(customers)
 	// 	})
-	db.each(`
-		SELECT FirstName || ' ' || LastName AS 'Name', Country, CustomerId FROM Customer
-		WHERE Country IS 'Brazil'`
-		, (err, { CustomerId, Name, Country }) => {
-			console.log(`${CustomerId}: ${Name} (${Country})`)
-	})
+	// db.each(`
+	// 	SELECT FirstName || ' ' || LastName AS 'Name', Country, CustomerId FROM Customer
+	// 	WHERE Country IS 'Brazil'`
+	// 	, (err, { CustomerId, Name, Country }) => {
+	// 		console.log(`${CustomerId}: ${Name} (${Country})`)
+	// })
+	// db.each(`
+	// 	SELECT InvoiceId, FirstName || ' ' || LastName AS 'Name', InvoiceDate, BillingCountry
+	// 	FROM Invoice
+	// 	JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+	// 	WHERE Customer.Country = 'Brazil'`
+	// 	, (err, row) => {
+	// 		let table = new cliTable({
+	// 			head: ['Invoice Id', 'Name', 'Invoice Date', 'Billing Country'],
+	// 			colWidths: [25, 25, 25, 25]
+	// 		})
+	// 		table.push([row.InvoiceId, row.Name, row.InvoiceDate, row.BillingCountry])
+	// 		console.log(table.toString())
+	// 	})
 
-	db.each(`
-		SELECT InvoiceId, FirstName || ' ' || LastName AS 'Name', InvoiceDate, BillingCountry
-		FROM Invoice
-		JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
-		WHERE Customer.Country = 'Brazil'`
-		, (err, row) => {
-			let table = new cliTable({
-				head: ['Invoice Id', 'Name', 'Invoice Date', 'Billing Country'],
-				colWidths: [25, 25, 25, 25]
-			})
-			table.push([row.InvoiceId, row.Name, row.InvoiceDate, row.BillingCountry])
-			console.log(table.toString())
-		})
-})
+	{
+    // 4. Provide a query showing only the Employees who are Sales Agents.
+    const head = ['Name']
+    const tbl = new cliTable({ head, style: { compact: true } })
+
+    db.each(`
+        SELECT FirstName || " " || LastName AS "Name"
+        FROM   Employee
+        WHERE  Employee.Title = "Sales Support Agent"
+      `,
+      (err, emp) => tbl.push([emp.Name]),
+      () => console.log(tbl.toString())
+    )
+  }
+ })
 
 db.close()
+
